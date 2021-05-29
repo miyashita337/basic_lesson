85d8e5eaad3cd8295199c4ee5d4d86d2340b1d61
hiroyuki_miyashita <hiroyuki_miyashita@pokelabo.co.jp>
2021-05-29 18:57:27 +0900

    useState study1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { counterSlice } from './features/counter/counterSlice';
 
-function App() {
+const  App: React.FC = () => {
+  const [status, setStatus] = useState<string | number>("text");
+  const [input, setInput] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [display, setDisplay] = useState(true);
+
+
+  const onChangeHundler =(e:React.ChangeEvent<HTMLInputElement>) =>{
+    setInput(e.target.value)
+  }
+  useEffect(()=> {
+    console.log("useEffect in App conp invoke")
+    document.title = `current value is ${counter}`;
+  },[counter]
+  );
+  //[] はマウントした時
+  //[counter]はcounter,だけのスコp−う
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h4>{status}</h4>
+        <button onClick={() => setStatus(1)}>button</button>
+        <h4>{input}</h4>
+        <input type="text" value={input} onChange={onChangeHundler}></input>
+        <h4>{counter}</h4>
+        <button onClick={() => setCounter((preCounter)=>preCounter+1)}>set counter</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/CleanUp.tsx
@@ -0,0 +1,12 @@
+//rafce利用
+import React from 'react'
+
+const CleanUp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CleanUp
